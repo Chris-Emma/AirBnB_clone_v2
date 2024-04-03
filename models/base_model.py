1a56bbd8a0763e91fd07c98e15e43bbdd129b452
Chris-Emma <chrisemmaofficial@gmail.com>
2024-04-03 18:01:28 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ import uuid
 from datetime import datetime
 import models
 from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
 
 
 class BaseModel:
